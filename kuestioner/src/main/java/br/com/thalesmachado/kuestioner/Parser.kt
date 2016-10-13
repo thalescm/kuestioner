a13e7bac48809eb5d03d7a6253aaf0b99178aebc
Thales Machado <thales.mchd@gmail.com>
2016-10-13 20:50:47 -0300

    Basic model parsing working

--- kuestioner/src/main/java/br/com/thalesmachado/kuestioner/Parser.kt
+++ kuestioner/src/main/java/br/com/thalesmachado/kuestioner/Parser.kt
@@ -1,11 +1,34 @@
 package br.com.thalesmachado.kuestioner
 
 class Parser {
-
     companion object {
 
-        fun parse (clazz: Class<out Kuestionable>) {
+        fun parse(clazz: Class<out Kuestionable>): String {
 
+            return "$bracketsStart${getClassName(clazz)}${getClassFields(clazz)}$bracketsEnd"
         }
     }
+}
+
+val bracketsStart = "{"
+val bracketsEnd = "}"
+
+fun classesWithoutFields (vararg classes : Class<*>) : Boolean {
+    return classes.all { it.declaredFields.size == 0 && it.fields.size == 0 }
+}
+
+fun getClassName(clazz: Class<*>): String = clazz.simpleName.toLowerCase()
+
+fun  getClassFields(clazz: Class<out Kuestionable>) : String {
+    if (classesWithoutFields(clazz)) return ""
+
+    var returnable = bracketsStart
+    clazz.fields.forEach {
+        returnable += it.name
+    }
+    clazz.declaredFields.forEach {
+        returnable += it.name
+    }
+    returnable.removeSuffix(" ")
+    return returnable + bracketsEnd
 }
--- kuestioner/src/test/java/br/com/thalesmachado/kuestioner/ParserTest.kt
+++ kuestioner/src/test/java/br/com/thalesmachado/kuestioner/ParserTest.kt
@@ -1,16 +1,16 @@
 package br.com.thalesmachado.kuestioner
 
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class ParserTest {
-
 
     @Test
     fun testBasicModelParsing () {
         val kuery = Parser.parse(Model::class.java)
 
         assertEquals(
-                "{model {name}}",
+                "{model{name}}",
                 kuery
         )
     }
