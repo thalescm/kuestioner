dda1342163f9b04279c1a8330808665093a06ef9
Thales Machado <thales.mchd@gmail.com>
2016-11-05 19:20:16 -0200

    Uploading to bintray on tags

--- sample/src/main/java/br/com/thalesmachado/sample/KuestionerSample.kt
+++ sample/src/main/java/br/com/thalesmachado/sample/KuestionerSample.kt
@@ -10,25 +10,23 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class KuestionerSample {
+fun main(args: Array<String>) {
 
-    init {
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.level = HttpLoggingInterceptor.Level.BODY
+    val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+    val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/")
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
 
-        val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/")
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
+    val service = retrofit.create(GithubService::class.java)
+    service
+            .query(ViewerQuery(Kuestioner.queryOn(Viewer::class.java)))
+            .subscribe(
+                    {println("SUCCESS") }
+                    , { println("ERROR") })
 
-        val service = retrofit.create(GithubService::class.java)
-        service
-                .query(ViewerQuery(Kuestioner.queryOn(Viewer::class.java)))
-                .subscribe(
-                        {println("SUCCESS") }
-                        , { println("ERROR") })
-    }
 }
